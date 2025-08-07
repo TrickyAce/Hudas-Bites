ccc5d417f8e0ce79ea835d05d3c7645532652400
TrickyAce <olaabeeb2040@gmail.com>
2025-08-07 18:17:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
         // Sample product data
         const products = [
-            { id: 1, name: '8"cake', price: "₦35,000", category: "cakes", image: "./images/product3.webp" },
+            { id: 1, name: '8 inches cake', price: "₦35,000", category: "cakes", image: "./images/product3.webp" },
             { id: 2, name: "Box of 12 Frosted Cupcakes", price: "₦20,000", category: "cupcakes", image: "./images/product2.webp" },
-            { id: 3, name: '10" Cake', price: "₦50,000", category: "cakes", image: "./images/product9.webp" },
+            { id: 3, name: '10 inches Cake', price: "₦50,000", category: "cakes", image: "./images/product9.webp" },
             { id: 4, name: "Cake Slice", price: "₦4,500", category: "cakes", image: "./images/product7.webp" },
             { id: 5, name: "Foil Cake", price: "₦3000", category: "cakes", image: "./images/product13.webp"},
             { id: 6, name: "Milk Candy", price: "₦2,500", category: "desserts", image: "./images/product1.webp" },
-            { id: 7, name: '6" Cake', price: "₦25,000", category: "cakes", image: "./images/product4.webp"},
+            { id: 7, name: '6 inches Cake', price: "₦25,000", category: "cakes", image: "./images/product4.webp"},
             { id: 8, name: "Bento Cake", price: "₦6,000", category: "cakes", image: "./images/product5.webp"},
             { id: 9, name: "Cake Parfait", price: "₦4,500", category: "desserts", image: "/images/product6.webp" },
             { id: 10, name: "Cinnamon Rolls", price: "₦3,000", category: "desserts", image: "./images/product8.webp"},
             { id: 11, name: "Plain Cupcakes", price: "₦15,000", category: "cupcakes", image: "./images/product10.webp"},
-            { id: 12, name: '6"cake', price: "₦25,00", category: "cakes", image: "./images/product14.webp"},
-            { id: 13, name: '6" Cake', price: "₦25,000", category: "cakes", image: "./images/product12.webp" },
+            { id: 12, name: '6 inches cake', price: "₦25,00", category: "cakes", image: "./images/product14.webp"},
+            { id: 13, name: '6 inches Cake', price: "₦25,000", category: "cakes", image: "./images/product12.webp" },
             { id: 14, name: "Tea Cake", price: "₦5,000", category: "desserts", image: "./images/product11.webp" }
         ];
 
